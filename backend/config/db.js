1854c51cabecb1bddc87e0ce30d6f78e4ad9abf3
Aman <amans27203@gmail.com>
2024-03-17 21:41:45 +0530

    db connection file

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,11 @@ const mongoose =require('mongoose');
 
 const dbConnection = () => mongoose.connect(process.env.DB_URI,{}).then(function(data){
     console.log(`db connected ${data.connection.host}`);
-}).catch(function(err){
-    console.log(err);
-})
+    })
 
+    // already handled error on the server.js file
+// .catch(function(err){
+//     // console.log(err);
+//     console.log('error in db connection');
+// })
 module.exports = dbConnection
