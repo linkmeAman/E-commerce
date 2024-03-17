054ab17200feeddaa62c8b3e7c0bc89d56a5e73d
Aman <amans27203@gmail.com>
2024-03-17 21:41:22 +0530

    main file for all middleware , routes , error handling

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,22 @@
 const express =  require('express');
+const errorMiddleware = require('./middleware/error');
 
 const app = express();
 
 app.use(express.json());
-// Routes imports
 
+
+// Routes imports
 const products = require('./routes/productsRoute');
 
 
 app.use("/api/v1",products);
-
-
-
-
-
 // console.log(app.get('/api/v1/products')); ;
 
 
+//imports for middleware Error handlers
+app.use(errorMiddleware);
+// console.log(errorMiddleware);
 
 
 module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,28 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const dbConnect = require('./config/db');
 
-
 dotenv.config({path:"backend/config/config.env"});
+// handling uncaught exception
+process.on('uncaughtException',(err) =>{
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Uncaught Exception");
+    process.exit(1);
+})
+
 
 dbConnect();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`listening on port  http://localhost:${process.env.PORT}`);
+})
+// console.log(dkdsk)
+
+// Unhandled Promise Rejection
+process.on('uncaughtException',(err) =>{
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Unhandled Promise Rejection");
+
+    server.close(()=>{
+        process.exit(1);
+    })
 })
