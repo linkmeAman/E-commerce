f6310752456bf312a89ed870615d921a0fd82038
Aman <amans27203@gmail.com>
2024-03-18 00:59:04 +0530

    added pagination function

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,36 +1,53 @@
 class apiFeatures {
-    constructor(query, queryStr) {
-        this.query = query;
-        this.queryStr = queryStr;
-    }
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
 
-    search() {
-        const keyword = this.queryStr.keyword ? {
-            $or: [
-                { name: { $regex: this.queryStr.keyword, $options: "i" } },
-                { description: { $regex: this.queryStr.keyword, $options: "i" } }
-            ]
-        } : {};
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          $or: [
+            { name: { $regex: this.queryStr.keyword, $options: "i" } },
+            { description: { $regex: this.queryStr.keyword, $options: "i" } },
+          ],
+        }
+      : {};
 
-        console.log(keyword);
+    console.log(keyword);
 
-        // Call the find function on the query object passed to the constructor
-        this.query = this.query.find({...keyword});
-        return this;
-    }
+    // Call the find function on the query object passed to the constructor
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
 
-    filter(){
-        const queryCopy = {...this.queryStr};
-        // console.log(queryCopy);
-        //removing some fields for category
-        const removeFields = ["keyword","page","limit"];
+  filter() {
+    const queryCopy = { ...this.queryStr };
+    // console.log(queryCopy);
+    //removing some fields for category
+    const removeFields = ["keyword", "page", "limit"];
 
-        removeFields.forEach(key=> delete queryCopy[key]);
-        // console.log(removeFields);
+    removeFields.forEach((key) => delete queryCopy[key]);
+    // console.log(removeFields);
 
-        this.query =this.query.find(queryCopy);
-        return this;
-    }
+    // Adding range filter for price and ratings
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    console.log(queryStr);
+    return this;
+  }
+
+  pagination(productsPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+
+    const skip = productsPerPage * (currentPage - 1);
+
+    this.query = this.query.limit(productsPerPage).skip(skip);
+
+    return this;
+  }
 }
 
 module.exports = apiFeatures;
