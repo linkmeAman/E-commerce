2cfc2c3399a3b876db0ad1f4b0dbe954cd19f77d
Aman <amans27203@gmail.com>
2024-03-17 21:40:04 +0530

    Routes file and path

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const { getAllProducts, CreateProducts } = require('../controllers/productsController');
+const { getAllProducts, createProducts ,updateProducts , deleteProducts , getSingleProducts } = require('../controllers/productsController');
 
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/products/new").post(CreateProducts);
+router.route("/products/new").post(createProducts);
+
+router.route("/products/:id").put(updateProducts).delete(deleteProducts).get(getSingleProducts); //  you can use both method as the url is same
+// router.route("/products/:id").put(updateProducts);
+// router.route("/products/:id").delete(deleteProducts);
+// router.route("/products/:id").get(getSingleProducts);
 
 
 
