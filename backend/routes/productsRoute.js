5fed5d8af36e1fb323b293a7003574040d1da194
Aman <amans27203@gmail.com>
2024-03-25 21:33:14 +0530

    change in routes

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const { getAllProducts, createProducts ,updateProducts , deleteProducts , getSingleProducts } = require('../controllers/productsController');
-const { isAuthenticateUser } = require('../middleware/auth');
+const { isAuthenticateUser, isAdminUser } = require('../middleware/auth');
 
 
 const router = express.Router();
 
-router.route("/products").get(isAuthenticateUser,getAllProducts);
-router.route("/products/new").post(createProducts);
+router.route("/products").get(isAuthenticateUser,isAdminUser('admin'),getAllProducts);
+router.route("/admin/products/new").post(isAuthenticateUser,isAdminUser('admin'),createProducts);
 
-router.route("/products/:id").put(updateProducts).delete(deleteProducts).get(getSingleProducts); //  you can use both method as the url is same
+router.route("/admin/products/:id").put(isAuthenticateUser,isAdminUser('admin'),updateProducts).delete(isAuthenticateUser,isAdminUser('admin'),deleteProducts); //  you can use both method as the url is same
 // router.route("/products/:id").put(updateProducts);
 // router.route("/products/:id").delete(deleteProducts);
 // router.route("/products/:id").get(getSingleProducts);
 
-
+router.route("/products/:id").get(getSingleProducts);
 
 module.exports = router;
