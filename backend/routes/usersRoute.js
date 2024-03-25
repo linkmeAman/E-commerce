f78f14bf9202a683ba346c069eb5cfc634c05984
Aman <amans27203@gmail.com>
2024-03-25 21:18:00 +0530

    Update userroute according to usercontroller

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,13 +1,31 @@
 const express= require('express');
-const {registerUsers , getAllUsers , updateUsers , deleteUsers ,loginUsers } = require ('../controllers/usersController');
+const {registerUsers , getAllUsers , updateUserPassword , deleteUsers ,loginUsers, logoutUser, resetPassword, forgotPassword, getUserDetails, updateUserProfile } = require ('../controllers/usersController');
+const { isAuthenticateUser, isAdminUser } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route("/users").get(getAllUsers);
-router.route("/users/new").post(registerUsers);
+// router.route("/users").get(getAllUsers);
+router.route("/users/new").post(isAdminUser,registerUsers);
 // router.route("/users/:id").put(updateUsers).delete(deleteUsers).get(getSingleUsers); //  you can use both method as the url is same
 
 //Login User
 router.route("/users/login").post(loginUsers);
+
+// User Deatails
+router.route("/details").get(isAuthenticateUser,getUserDetails);
+// Logout User
+router.route("/users/logout").post(logoutUser);
+
+// forgot Password and Reset Password
+router.route("/users/resetPassword").post(forgotPassword);
+
+router.route("/password/reset/:resetToken").put(resetPassword);
+
+router.route("/users/password/update").put(isAuthenticateUser,updateUserPassword);
+
+router.route("/users/update").put(isAuthenticateUser, updateUserProfile);
+
+
+
 
 module.exports = router;
