580c04f90c39d1dc3d31dc8f5bb1633d5e9fea23
Aman <amans27203@gmail.com>
2024-03-31 15:08:47 +0530

    added routes for specific api calls

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const { getAllProducts, createProducts ,updateProducts , deleteProducts , getSingleProducts } = require('../controllers/productsController');
-const { isAuthenticateUser, isAdminUser } = require('../middleware/auth');
-
+const express = require("express");
+const {
+  getAllProducts,
+  createProducts,
+  updateProducts,
+  deleteProducts,
+  getSingleProducts,
+  creatingProductReviews,
+  getProductReviews,
+  deleteReview,
+} = require("../controllers/productsController");
+const { isAuthenticateUser, isAdminUser } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/products").get(isAuthenticateUser,isAdminUser('admin'),getAllProducts);
-router.route("/admin/products/new").post(isAuthenticateUser,isAdminUser('admin'),createProducts);
+router
+  .route("/products")
+  .get(isAuthenticateUser, isAdminUser("admin"), getAllProducts);
+router
+  .route("/admin/products/new")
+  .post(isAuthenticateUser, isAdminUser("admin"), createProducts);
 
-router.route("/admin/products/:id").put(isAuthenticateUser,isAdminUser('admin'),updateProducts).delete(isAuthenticateUser,isAdminUser('admin'),deleteProducts); //  you can use both method as the url is same
+router
+  .route("/admin/products/:id")
+  .put(isAuthenticateUser, isAdminUser("admin"), updateProducts)
+  .delete(isAuthenticateUser, isAdminUser("admin"), deleteProducts); //  you can use both method as the url is same
 // router.route("/products/:id").put(updateProducts);
 // router.route("/products/:id").delete(deleteProducts);
 // router.route("/products/:id").get(getSingleProducts);
 
 router.route("/products/:id").get(getSingleProducts);
+
+router.route("/reviews").put(isAuthenticateUser, creatingProductReviews );
+
+router.route("/review").get(isAuthenticateUser, getProductReviews).delete(isAuthenticateUser,isAdminUser('admin'),deleteReview);
+
 
 module.exports = router;
