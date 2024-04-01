a6028c55ed37a30c1b1b3bfdc4b7286bbebde418
Aman <amans27203@gmail.com>
2024-04-01 22:18:21 +0530

    feat(Install React-app): :tada: Created frontend named react app using pnpm with vite on typescript for better performance

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import Header from "./component/layout/Header.tsx"
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App
