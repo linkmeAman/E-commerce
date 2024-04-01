bd6c892c893e90a0b74991813f9ff50a97088e2c
Aman <amans27203@gmail.com>
2024-04-02 02:51:56 +0530

    Created footer and header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 // import { useState } from 'react'
-import Header from "./component/layout/Header.tsx"
-
+import { useState } from "react"
+import Header from "./component/layout/Header/Header"
+import Footer from "./component/layout/Footer/Footer"
+import { BrowserRouter as Router } from "react-router-dom"
+import WebFont from "webfontloader"
 function App() {
-
+  useState(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    })
+  })
   return (
     <>
-    <Header />
+      <Router>
+        <Header />
+        <Footer />
+      </Router>
     </>
   )
 }
